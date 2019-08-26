f4d80ea4a2812b9aaeaa28f64202bad0e9b5aa4f
xj894976677 <894976677@qq.com>
2019-08-26 11:20:56 +0800

    更新模板

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'helloWorld'
+    user = {'username': 'Miguel'}  # 用户
+    posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user='Netop', posts=posts)
