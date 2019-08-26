f1159a456a9ca787703fec308ae082a87cff046c
xj894976677 <894976677@qq.com>
2019-08-26 19:12:51 +0800

    配置flask shell上下文对象

--- blog.py
+++ blog.py
@@ -1,4 +1,11 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+# 配置flask上下文  让程序flask run之后可以执行flask shell中调用返回的字典中的数据
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
