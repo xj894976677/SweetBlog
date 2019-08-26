446bfe7cf47f74172c06932fd4b13b045ac598e6
xj894976677 <894976677@qq.com>
2019-08-26 19:13:50 +0800

    加入数据库迁移

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
 from flask import Flask
-app = Flask(__name__)
-print('谁在使用我', __name__)
+# 从config导入Config类，配置app
+from config import Config
+# 导入SQLAlchemy 操作数据库
+from flask_sqlalchemy import SQLAlchemy
+# 导入Migrate 为了数据库迁移
+from flask_migrate import Migrate
 
-from app import routes
+
+# 创建app实例
+app = Flask(__name__)
+# 通过config类配置app
+app.config.from_object(Config)
+# 创建SQLAlchemy实例db,并绑定app
+db = SQLAlchemy(app)
+# 创建迁移引擎对象
+migrate = Migrate(app, db)
+
+
+# 引入路由文件与数据库模型类，放在最后防止与其文件循环引用
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
-from flask import render_template
-
+from flask import render_template, flash, redirect
+from app import forms
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}  # 用户
+    # user = {'username': 'Miguel'}  # 用户
     posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
         {
             'author': {'username': 'John'},
@@ -18,3 +18,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user='Netop', posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
