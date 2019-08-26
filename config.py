3f5865a4e15ffe936c61e8325f1c92e1e14b1282
xj894976677 <894976677@qq.com>
2019-08-26 19:13:19 +0800

    更新app的配置类

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+basedir = os.path.dirname(__file__)
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
+    # 配置sqlite数据库
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///'+os.path.join(basedir, 'database.sqlite')
+    # 配置不需要flask-SQLALCHEMY的该特征，即追踪对象的修改并且发送信号
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
